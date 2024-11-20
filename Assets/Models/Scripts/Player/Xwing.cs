20d20989fe1db4154583231debb8fec1fffb2889
aledifon <aledifon89@gmail.com>
2024-11-20 14:10:32 +0100

    Implemented startship movement and turning through Transform Component

--- Assets/Models/Scripts/Player/Xwing.cs
+++ Assets/Models/Scripts/Player/Xwing.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Runtime.ConstrainedExecution;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.Rendering;
 
 public class Xwing : MonoBehaviour
-{    
+{
     // Global vars of the class
+    [Header("Movement")]
     [SerializeField]
-    //[Header("Speed")]
     private int speed;
+    [SerializeField]
+    private int turnSpeed;
+    [SerializeField]
+    private int speedBoost = 10;
 
     private float horizontal,
-                    vertical;
+                    vertical,
+                    xMouse,
+                    yMouse;
+
+    private Rigidbody rb;
+    private int moveSpeed;
 
     private void Awake()
     {
@@ -23,28 +34,64 @@ public class Xwing : MonoBehaviour
     
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        moveSpeed = speed;
     }
     
     void Update()
     {
         InputPlayer();
+        //Spin();
+        Movement();
+        Turning();
     }
 
-    //private void FixedUpdate()
-    //{
-        
-    //}
+    private void FixedUpdate()
+    {
+        //InputPlayer();
+        //Spin();
+        //MovementPhysics();
+        //Turning();
+    }
 
     // To move my starship in the x-axis and z-axis.
     private void Movement()
     {
-        
-    }
+        Vector3 direction = new Vector3(horizontal, 0, vertical);                
 
+        transform.Translate(direction.normalized * moveSpeed * Time.deltaTime);        
+    }
+    private void Turning()
+    {
+        Vector3 rotation = new Vector3(-yMouse, xMouse,0);
+
+        transform.Rotate(rotation.normalized * turnSpeed * Time.deltaTime);
+    }
+    private void MovementPhysics()
+    {
+        Vector3 inputPlayerDirection = new Vector3(horizontal, 0, vertical);        
+
+        //rb.MovePosition(rb.position + inputPlayerDirection.normalized * Time.fixedDeltaTime * moveSpeed);
+        rb.AddForce(inputPlayerDirection.normalized * Time.fixedDeltaTime * moveSpeed, ForceMode.Impulse);
+    }
     private void InputPlayer()    
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+        xMouse = Input.GetAxis("Mouse X");
+        yMouse = Input.GetAxis("Mouse Y");
+    }
+    void Spin()
+    {
+        if (Input.GetMouseButtonDown(1) && horizontal != 0)        
+        {
+            moveSpeed = speed * speedBoost;
+            StartCoroutine(SetDefaultSpeedAfterDelay(0.2f));
+        }
+    }
+    IEnumerator SetDefaultSpeedAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);         // Wait for a delay        
+        moveSpeed = speed;                              // Set again the default Movement Speed
     }
 }
